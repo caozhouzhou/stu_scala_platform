5f6d50e0198c90be414e191ecda34d1d6ead7509
caozhou_zch <caozhou_zch@163.com>
2019-11-12 22:04:17 +0800

    获取所有用户

--- src/main/java/com/jdwa/service/UserServiceImpl.java
+++ src/main/java/com/jdwa/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.jdwa.service;
 
+import com.jdwa.dao.UserMapper;
 import com.jdwa.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +12,10 @@ import java.util.List;
  */
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper mapper;
     public List<User> findUsers(){
-        return null;
+        return mapper.selectByExample(null);
     }
 }
